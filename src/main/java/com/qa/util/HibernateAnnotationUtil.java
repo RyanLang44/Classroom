98d0ccb76b6ce1d98614989d6206e0bae58bea6f
RyanLang44 <ryanlang44@gmail.com>
2019-02-15 14:31:12 +0000

    doesnt work atm

---
+++ src/main/java/com/qa/util/HibernateAnnotationUtil.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class HibernateAnnotationUtil {
+
+	public HibernateAnnotationUtil() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO CLASSROOM (classroomID, trainer) VALUES ('1, 'Matt')
-INSERT INTO CLASSROOM (classroomID, trainer) VALUES ('2, 'Jordan')
+INSERT INTO CLASSROOM (classroomID, trainer, trainees) VALUES ('1, 'Matt')
+INSERT INTO CLASSROOM (classroomID, trainer,trainees) VALUES ('2, 'Jordan')
 INSERT INTO TRAINEE (TraineeID, traineeName) VALUES (1, 'Ryan')
 INSERT INTO TRAINEE (TraineeID, traineeName) VALUES (2, 'Josh')
